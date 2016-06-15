5f1d75f33742bcd4c6f220eb4ad1c0998866dd73
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2016-06-15 16:47:42 +0200

    Slim-Ify
    Change-Id: Id9646751eab8850a4681b94216edfb9f9d54b32d
    Signed-off-by: Gianmarco Scarano <gianmarcoscarano@gmail.com>

---
+++ slim.mk
@@ -0,0 +1,14 @@
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
+
+# Release name
+PRODUCT_RELEASE_NAME := LG L90
+PRODUCT_NAME := slim_w7
+PRODUCT_MANUFACTURER := lge
+PRODUCT_MODEL := w7
+
+$(call inherit-product, device/lge/w7/full_w7.mk)
