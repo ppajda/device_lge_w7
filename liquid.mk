5803c3980851e3255ba51e0d0c31961cd3e4de89
mosimchah <mosimchah@gmail.com>
2016-07-11 17:33:44 -0400

    Update and rename broken.mk to liquid.mk

---
+++ liquid.mk
@@ -0,0 +1,14 @@
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
+
+# Release name
+PRODUCT_RELEASE_NAME := LG L90
+PRODUCT_NAME := liquid_w7
+PRODUCT_MANUFACTURER := lge
+PRODUCT_MODEL := w7
+
+$(call inherit-product, device/lge/w7/full_w7.mk)
