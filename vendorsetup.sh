f8f8e419b6b45dbd2e256111a08a4370f0c0f093
mosimchah <mosimchah@gmail.com>
2016-07-21 14:11:06 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo turbo_w7-userdebug
+add_lunch_combo tesla_w7-userdebug
 
