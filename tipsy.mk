b62ad217dc84072ea17aadda07c8d7852a0bc74c
ppajda <pajdek@gmail.com>
2016-09-29 10:32:56 +0200

    Update and rename tesla.mk to tipsy.mk

--- tipsy.mk
+++ tipsy.mk
@@ -1,5 +1,5 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 540
@@ -7,7 +7,7 @@ TARGET_SCREEN_HEIGHT := 960
 
 # Release name
 PRODUCT_RELEASE_NAME := LG L90
-PRODUCT_NAME := tesla_w7
+PRODUCT_NAME := tipsy_w7
 PRODUCT_MANUFACTURER := lge
 PRODUCT_MODEL := w7
 
